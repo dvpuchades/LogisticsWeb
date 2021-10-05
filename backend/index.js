c6ff295e6303cb0a373b874532f7c61fcd5dc5c3
dvpuchades <dapucto@upv.edu.es>
2021-10-05 19:25:06 +0100

    working...

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const app = express()
+
 const authRoute = require('./routes/auth')
+const routes = require('./routes/routes')
 
 const  dbURI = "mongodb://localhost/authentication"
 app.use(express.json())
 app.use('/api/auth', authRoute)
-
+app.use('/api', routes)
 
 mongoose.connect(dbURI , {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
@@ -14,6 +16,6 @@ const db = mongoose.connection
 db.on("error", (err)=>{console.error(err)})
 db.once("open", () => {console.log("DB started successfully")})
 
-//TODO
+// TODO
 
 app.listen(2400, () => {console.log("Server started: 2400")})
---
+++ backend/routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+
+});
+
+router.post('/delivery', (req, res) => {
+    
+});
+
+module.exports = router
