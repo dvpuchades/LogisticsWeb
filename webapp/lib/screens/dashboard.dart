ff0bddc319a17d91e82b1cd201c2dcb87ef33f25
dvpuchades <dapucto@upv.edu.es>
2021-12-08 16:10:13 +0000

    working...

--- webapp/lib/screens/dashboard.dart
+++ webapp/lib/screens/dashboard.dart
@@ -24,21 +24,64 @@ class Dashboard extends StatelessWidget {
                       ),
                       height: 100),
                   Expanded(
-                      child: ListView(shrinkWrap: true, children: [
-                    const UserCard(
-                        name: 'Declan Simons', minutes: 39, deliveries: 4),
-                    const UserCard(
+                      child: ListView(shrinkWrap: true, children: const [
+                    UserCard(name: 'Declan Simons', minutes: 39, deliveries: 4),
+                    UserCard(
                         name: 'Melissa Rodrigues', minutes: 25, deliveries: 7),
-                    const UserCard(
-                        name: 'Jamie Almond', minutes: 21, deliveries: 5),
-                    const UserCard(
+                    UserCard(name: 'Jamie Almond', minutes: 21, deliveries: 5),
+                    UserCard(
                         name: 'Laurence Knieve', minutes: 19, deliveries: 1),
-                    const UserCard(
-                        name: 'Paul Semper', minutes: 3, deliveries: 9),
+                    UserCard(name: 'Paul Semper', minutes: 3, deliveries: 9),
                   ])),
                 ]))),
         Expanded(
-          child: Container(color: Colors.white),
+          child: Column(
+            children: [
+              Flexible(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Flexible(
+                      child: Container(
+                          margin: const EdgeInsets.only(top: 30),
+                          child: SizedBox(
+                              height: 50,
+                              width: 200,
+                              child: Container(
+                                color: ThemeColors.black,
+                                child: TextButton(
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: const [
+                                        Icon(
+                                          Icons.add,
+                                          size: 30,
+                                          color: ThemeColors.white,
+                                        ),
+                                        Text('New Order',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                color: ThemeColors.white))
+                                      ]),
+                                  onPressed: () {
+                                    //TODO
+                                  },
+                                ),
+                              )))),
+                  Flexible(
+                      child: Container(
+                          margin: const EdgeInsets.only(
+                              top: 30, left: 20, right: 30),
+                          child: SizedBox(
+                              height: 50,
+                              width: 200,
+                              child: Container(color: ThemeColors.black))))
+                ],
+              )), //new order and profile buttons
+              Spacer(flex: 5)
+            ],
+          ),
           flex: 4,
         )
       ],
